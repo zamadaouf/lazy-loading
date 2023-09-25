356cee18826e20108c57c546fde0b38aef55711b
Zineb AMADAOUF <z.amadaouf@aevaweb.com>
2023-09-25 08:38:36 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GewdLazyLoaderModule } from '@gewd/lazy';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GewdLazyLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
